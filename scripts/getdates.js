387765eb1600cd3aa3ce4cff85c2c0fe3c263d10
Vladimir <vovyasychugov@gmail.com>
2024-01-06 14:14:22 +0000

    new things

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,12 @@ yearSpan.textContent = currentYear;
 const lastModifiedSpan = document.getElementById('lastModified');
 const lastModified = document.lastModified;
 lastModifiedSpan.textContent = `Last Modification: ${lastModified}`;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
