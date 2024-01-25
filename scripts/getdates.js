296943512787b60988583df863ea3608d5f0bdcf
Vladimir <vovyasychugov@gmail.com>
2024-01-25 20:17:29 +0000

    adding map and hamburger menu to my web page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,3 +11,14 @@ date.innerHTML = new Date().toLocaleDateString("en-US", options);
 
 lastModified.innerHTML = (`Last modification ${document.lastModified}`);
 
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
+
