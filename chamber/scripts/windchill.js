9d5ead547b938188c52179c5ce9b64c8688e9006
Vladimir <vovyasychugov@gmail.com>
2024-02-20 22:03:01 +0000

    working on wind chill task

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,18 @@
-const temperature = document.querySelector('.temperature').value;
-const wind_speed = document.querySelector('.wind-speed').value;
-const btn = document.querySelector('.wind-btn');
+
+function chil() {
+const temperature = document.querySelector('.temperature').value*1.8+32;
+const wind_speed = document.querySelector('.wind-speed').value/1.609;
+// const btn = document.querySelector('.wind-btn');
 
 
-btn.addEventListener('onclick', () => {
-    if (temperature <= 50 && wind_speed > 3.0 ) {
-        var windChill = (35.74 + (0.6215 * temperature))-(35.75 * Math.pow(wind_speed,0.16)) + (0.4275* temperature * Math.pow(wind_speed,0.16));
-   } else {
-       var windChill = 'N/A';
-   }  
-});
+// btn.addEventListener('onclick', () => {
+    let windchill_fahr = 35.74 + 0.6215*temperature - 35.75*(wind_speed**0.16) + 0.4275*temperature*(wind_speed**0.16);
+    let windchill_cels = (windchill_fahr - 32)*5/9;
+if (temperature<=50 && wind_speed>3){
+    document.querySelector('.windchill').innerHTML = Math.round(windchill_cels) + ' ℃';
+} 
+    else{
+        document.querySelector('.windchill').innerHTML = ' N/A';
+    }
 
-
-var windChill = Math.round(windChill);
-document.getElementById('windChill').innerHTML = windChill;
+};
