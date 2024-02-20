bca3d88d05a938346bf9aaf14f948e15f1804f3b
Vladimir <vovyasychugov@gmail.com>
2024-02-20 22:03:11 +0000

    and again

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,18 +1,13 @@
+const temperature = document.querySelector('.temp').textContent;
+const wind_speed = document.querySelector('.wSpeed').textContent;
+console.log(wind_speed);
+console.log(temperature);
 
-function chil() {
-const temperature = document.querySelector('.temperature').value*1.8+32;
-const wind_speed = document.querySelector('.wind-speed').value/1.609;
-// const btn = document.querySelector('.wind-btn');
 
+var windChill= (35.74 + (0.6215 * temperature))-(35.75 * Math.pow(wind_speed,0.16))
++ (0.4275*temperature*Math.pow(wind_speed,0.16));
 
-// btn.addEventListener('onclick', () => {
-    let windchill_fahr = 35.74 + 0.6215*temperature - 35.75*(wind_speed**0.16) + 0.4275*temperature*(wind_speed**0.16);
-    let windchill_cels = (windchill_fahr - 32)*5/9;
-if (temperature<=50 && wind_speed>3){
-    document.querySelector('.windchill').innerHTML = Math.round(windchill_cels) + ' ℃';
-} 
-    else{
-        document.querySelector('.windchill').innerHTML = ' N/A';
-    }
+var windChill= Math.round(windChill);
 
-};
+console.log(windChill);
+document.querySelector(".weather").textContent= `The wind chill is : ${windChill}`;
